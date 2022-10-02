794d0f91d93f27ffac99beffaf79fd0ff902396e
fuyucn <ryan.fuyu@gmail.com>
2022-10-02 15:54:58 -0700

    feat(route): route

---
+++ src/app.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+  </div>
+</template>
+
+<style global>
+html,
+body,
+#__nuxt,
+#__layout {
+  height: 100%;
+  width: 100%;
+  padding: 0;
+  margin: 0;
+}
+* {
+  box-sizing: border-box;
+}
+</style>
---
+++ src/pages/new.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+const content = ref('')
+
+</script>
+<template>
+<div> new note</div>
+</template>
